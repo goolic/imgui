080ea17a7e17e837e5b16ec60936c36b49444274
André Villar <goolic@gmail.com>
2020-07-09 12:21:41 -0300

    more build and .sln changes

--- examples/example_win32_directx12/build_win32.bat
+++ examples/example_win32_directx12/build_win32.bat
@@ -13,25 +13,20 @@ set compiler_options=/nologo /Zi /MD /I .. /I ..\.. /I "%WindowsSdkDir%Include\u
 @REM set debug_options=
 set debug_options=/FeDebug/example_win32_directx12.exe /FoDebug/
 set link_options=/link d3d12.lib d3dcompiler.lib dxgi.lib
-rem if exist brcalc.pch (
+rem if exist mainnnn.pch (
 rem 	rem use pch
 rem 	echo usando pch
-rem    cl  %compiler_options% /Yu brcalc.pch main.cpp %debug_options% %link_options%
+rem    cl  %compiler_options% /Yu main.pch main.cpp %debug_options% %link_options%
 rem ) else (
 rem    rem create pch
 rem    echo nao usando pch
-rem 	cl %compiler_options%      ..\imgui_impl_dx12.cpp ^
-rem                               ..\imgui_impl_win32.cpp ^
-rem                               ..\..\imgui.cpp ^
-rem                               ..\..\imgui_demo.cpp ^
-rem                               ..\..\imgui_draw.cpp ^
-rem                               ..\..\imgui_widgets.cpp ^
-rem                           /Ycmain.cpp /Fpbrcalc.pch %debug_options% %link_options%
+rem    cl %compiler_options%  /Yc pch.cpp ^
+rem                           %debug_options% %link_options% /ENTRY:mainCRTStartup 
 rem )
 cl %compiler_options% ..\imgui_impl_dx12.cpp ^
                       ..\imgui_impl_win32.cpp ^
-                      ..\..\imgui.cpp ^
-                      ..\..\imgui_demo.cpp ^
+                       ..\..\imgui.cpp ^
+                       ..\..\imgui_demo.cpp ^
                       ..\..\imgui_draw.cpp ^
                       ..\..\imgui_widgets.cpp ^
                       main.cpp ^
